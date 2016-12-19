0b17b4316b70433c6591eda369c037e32ae04bb7
Le Deng <ray.deng83@gmail.com>
2016-12-18 23:53:41 -0500

    first commit

---
+++ src/main/java/com/bookstore/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.bookstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by lede on 12/18/16.
+ */
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "index";
+    }
+}

350611135cd4e94ee5e004a9194247ba6b119b2d
Le Deng <z00382545@z00382545-mac.corp.ad.fmcna.com>
2016-12-20 16:16:47 -0500

    start to add backend

---
+++ src/main/java/com/bookstore/repository/UserRepo.java
@@ -0,0 +1,16 @@
+package com.bookstore.repository;
+
+import com.bookstore.domain.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+/**
+ * Created by z00382545 on 12/20/16.
+ */
+public interface UserRepo extends CrudRepository<User, Long> {
+
+    User findByUsername(String username);
+    User findByEmail(String email);
+    List<User> findAll();
+}

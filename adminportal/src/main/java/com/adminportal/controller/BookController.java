01dfefc96a8d555659a1e6354d008a7b94fbc3de
Le Deng <z00382545@z00382545-mac.local>
2016-12-26 10:22:19 -0500

    latest

--- adminportal/src/main/java/com/adminportal/controller/BookController.java
+++ adminportal/src/main/java/com/adminportal/controller/BookController.java
@@ -14,6 +14,7 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
@@ -87,5 +88,39 @@ public class BookController {
         model.addAttribute("book", book);
 
         return "bookInfo";
+    }
+
+    @RequestMapping("/updateBook")
+    public String updateBook(@RequestParam("id") Long id, Model model){
+        Book book = bookService.findOne(id);
+        model.addAttribute("book", book);
+
+        return "updateBook";
+    }
+
+    @RequestMapping(value = "/updateBook", method = RequestMethod.POST)
+    public String updateBookPost(@ModelAttribute("book") Book book, HttpServletRequest request) {
+        bookService.save(book);
+
+        MultipartFile bookImage = book.getBookImage();
+
+//        if(bookImage != null) {
+//            try {
+//
+//                byte[] bytes = bookImage.getBytes();
+//                String name = book.getId() + ".png";
+//
+//                Files.delete(Paths.get("src/main/resources/static/image/book/"+name));
+//
+//                BufferedOutputStream stream =
+//                        new BufferedOutputStream(new FileOutputStream(new File("src/main/resources/static/image/book/"+name)));
+//                stream.write(bytes);
+//                stream.close();
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
+
+        return "redirect:/book/bookInfo?id="+book.getId();
     }
 }
